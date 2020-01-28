52efbd8ef691efac8fbb4574ae57bb66c90446f1
Jander <janderrv26@gmail.com>
2020-01-28 19:58:04 -0400

    Deletar categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,9 +1,49 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("./Category");
+const slugify = require("slugify");
 
-router.get("/categories", (req,res)=>{
-    res.send("rota categorias");
+router.get("/admin/categories/new", (req, res) => {
+    res.render("admin/categories/new");
 });
 
+router.post("/categories/save", (req, res) => {
+    var title = req.body.title;
+    if (title != undefined) {
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() => {
+            res.redirect("/");
+        });
+    } else {
+        res.redirect("/admin/categories/new");
+    }
+});
+
+router.get("/admin/categories", (req, res) => {
+    Category.findAll().then(categories => {
+        res.render("admin/categories/index", { categories: categories });
+    });
+});
+
+router.post("/categories/delete", (req, res) => {
+    var id = req.body.id;
+    if (id != undefined) {
+        if (!isNaN(id)) {//checa se id é numero
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/categories");
+            });
+        } else {//nao e numero
+            res.redirect("/admin/categories");
+        }
+    } else {//null
+        res.redirect("/admin/categories");
+    }
+});
 
 module.exports = router;
