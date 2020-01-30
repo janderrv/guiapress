c2b5c660e2b83aaaaa92a2c01b2c96883a45017f
Jander <janderrv26@gmail.com>
2020-01-30 00:03:40 -0400

    Paginação

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -59,7 +59,7 @@ router.get("/admin/articles/edit/:id", (req, res) => {
     if (isNaN(id)) {
         res.redirect("/admin/articles");
     } else {
-        Article.findByPk(id,{
+        Article.findByPk(id, {
             include: [{ model: Category }]
         }).then(article => {
             if (article != undefined) {
@@ -89,5 +89,37 @@ router.post("/articles/update", (req, res) => {
         res.redirect("/admin/articles");
     });
 });
+
+router.get("/articles/page/:num", (req, res) => {
+    var page = req.params.num;
+    var offset = 0;
+
+    if (isNaN(page) || page == 1) {
+        offset = 0;
+    } else {
+        offset = (parseInt(page) - 1) * 4;
+    }
+
+    Article.findAndCountAll({
+        limit: 4,
+        offset: offset, 
+        order: [['id', 'DESC']]
+    }).then(articles => {
+        var next;
+        if (offset + 4 >= articles.count) {
+            next = false;
+        } else {
+            next = true;
+        }
+        var result = {
+            page: parseInt(page),
+            next: next,
+            articles: articles
+        }
+        Category.findAll().then(categories => {
+            res.render("admin/articles/page", { result: result, categories: categories });
+        })
+    });
+})
 
 module.exports = router;
