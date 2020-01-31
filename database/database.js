730a5b6af1e89876008169f1c29b1c83e93a21e2
Jander <janderrv26@gmail.com>
2020-01-31 19:26:02 -0400

    correção erro

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize("guiapressbd", "janderrv", "ypvsdt200", {
-    host: "mysql669.umbler.com",
+const connection = new Sequelize("guiapress", "root", "root", {
+    host: "localhost",
     dialect: "mysql",
     timezone: "-03:00"
 });
